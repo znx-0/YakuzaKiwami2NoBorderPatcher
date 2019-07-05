590896d5b6c3be8a418eff0435c43ec002056135
zypnyx <rohitworkmail8@gmail.com>
2019-07-06 02:52:27 +0530

    Initial Commit

---
+++ YakuzaPatcher/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace YakuzaPatcher
+{
+    partial class YakuzaKiwami2NoBarsPatcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YakuzaKiwami2NoBarsPatcher));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(459, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(308, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Patch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(18, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(85, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Info : Choose \"ui.par\" file";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(15, 385);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(434, 17);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Source Code: https://github.com/zypnyx/YakuzaKiwami2NoBorderPatcher";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // YakuzaKiwami2NoBarsPatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "YakuzaKiwami2NoBarsPatcher";
+            this.Text = "YakuzaKiwami2NoBarsPatcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ YakuzaPatcher/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace YakuzaPatcher
+{
+    public partial class YakuzaKiwami2NoBarsPatcher : Form
+    {
+        OpenFileDialog openFile = new OpenFileDialog();
+
+        String FileName;
+        bool isOK = true;
+
+        public YakuzaKiwami2NoBarsPatcher()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(FileName))
+            {
+                label1.Text = "Error : Please choose ui.par file.";
+                label1.ForeColor = Color.Red;
+            }
+            else
+            {
+                label1.Text = "Info : Patching ...";
+                label1.ForeColor = Color.Blue;
+
+                Patch();
+
+                if (isOK)
+                {
+                    label1.Text = "Info : Patching Complete";
+                    label1.ForeColor = Color.Blue;
+
+                    label1.Text = "Info : Please Copy generated ui.par file to game folder (\\Yakuza Kiwami 2 \\ data)";
+                    label1.ForeColor = Color.Blue;
+
+                }
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                if (openFile.FileName.Contains("ui.par"))
+                {
+                    FileName = openFile.FileName;
+                    label1.Text = "Info : "  + FileName;
+                    label1.ForeColor = Color.Blue;
+                }
+                else
+                {
+                    label1.Text = "Error : Please choose ui.par file only.";
+                    label1.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Patch()
+        {
+            if(!String.IsNullOrEmpty(FileName))
+            { 
+                String command = "-d -f -s \"" + FileName + "\" nobars-v_1_3 ui.par";
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("xdelta3.exe", command);
+
+                procStartInfo.RedirectStandardError = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+
+                // wrap IDisposable into using (in order to release hProcess) 
+                using (Process process = new Process())
+                {
+                    process.StartInfo = procStartInfo;
+                    process.Start();
+
+                    // Add this: wait until process does its work
+                    process.WaitForExit();
+
+                    // and only then read the result
+                    string result = process.StandardError.ReadToEnd();
+                    if(!String.IsNullOrEmpty(result))
+                    {
+                        isOK = false;
+                        label1.Text = result;
+                        label1.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        isOK = true;
+                    }
+                }
+            }
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/zypnyx/YakuzaKiwami2NoBorderPatcher");
+
+        }
+    }
+}
